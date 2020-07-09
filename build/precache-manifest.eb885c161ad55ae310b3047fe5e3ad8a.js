3ec8086149963a2918e17e4d234d769a404f2584
AkselDjun <akseldjun@gmail.com>
2020-07-09 18:21:18 +0300

    finish project & firebase deploy

---
+++ build/precache-manifest.eb885c161ad55ae310b3047fe5e3ad8a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56481d268d5365dc66ff07ee985526ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76d8f49fe52173882268",
+    "url": "/static/css/main.2cc7c533.chunk.css"
+  },
+  {
+    "revision": "b1ddac58597943e9b563",
+    "url": "/static/js/2.a4aa9e11.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a4aa9e11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76d8f49fe52173882268",
+    "url": "/static/js/main.35a28484.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
